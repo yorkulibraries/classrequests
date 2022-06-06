8ace8320e7802f030c82c0b69d9d59f2c89d3bc0
Ali Sadaqain YorkU <sadaqain@yorku.ca>
2022-06-06 09:48:17 -0400

    changed role id for admin and added staff_admin for future

--- app/models/staff_profile.rb
+++ app/models/staff_profile.rb
@@ -11,6 +11,6 @@ class StaffProfile < ApplicationRecord
   # admin = 4
 
   # enum role: %i[not_set student_staff staff_instructor manager administrator]
-  enumerize :role, in: { not_set: 0, student_staff: 1, staff_instructor: 2, manager: 3, administrator: 4}, default: :not_set
+  enumerize :role, in: { not_set: 0, student_staff: 1, staff_instructor: 2, staff_admin: 3, manager: 4, administrator: 9}, default: :not_set
 
 end
